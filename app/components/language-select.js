555546dcfc58b72bf034c899c51c7eb3ba3b76ee
Richie Thomas <richie.thomas@blueapron.com>
2017-06-17 16:23:30 -0400

    Settings menu now has a (buggy) dropdown menu with 3 languages, and selecting a language will change the user's locale to that language and close the settings box; still lots of bugs to work out

--- app/components/language-select.js
+++ app/components/language-select.js
@@ -4,44 +4,32 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   config: Ember.inject.service(),
   i18n: Ember.inject.service(),
-  languages: Ember.computed(function() {
-    return ['foo', 'bar', 'baz'];
+  languageOptions: Ember.computed(function() {
+    return Object.keys(this.get('languageMap'));
+  }),
+  onFinish: null,
+  languageMap: Ember.computed(() => {
+    return {
+      "English": "en",
+      "French": "fr",
+      "Spanish": "es"
+    }
   }),
   selectedLanguage: null,
 
-  didRender() {
-    // let configDB = this.get('config.configDB');
-    // let langChoice;
-    // configDB.get('current_user').then((user) => {
-    //   configDB.put({
-    //     lang: 'en',
-    //     _id: user._id,
-    //     _rev: user._rev,
-    //     value: user.value
-    //   });
-    //   langChoice = 'en';
-    //   this.set('i18n.locale', user.lang);
-    //   debugger;
-    // });
-    // // let user = configDB.get('current_user').then((user) => {
-    // debugger;
-      // user.set('lang', 'en');
-    // });
-
-  },
-
   actions: {
-    selectLanguage() {
+    selectLanguage(_, selection) {
       let configDB = this.get('config.configDB');
+      let languageChoice = this.get('languageMap')[selection];
     configDB.get('current_user').then((user) => {
       configDB.put({
-        lang: 'es',
+        lang: languageChoice,
         _id: user._id,
         _rev: user._rev,
         value: user.value
       });
       this.set('i18n.locale', user.lang);
-      // debugger;
+      this.get('onFinish')();
     });
     }
   }
