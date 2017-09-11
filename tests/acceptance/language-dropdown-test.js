1e72e7e57ff13a519ff8c8a9003fdee295ba4592
Richie Thomas <richie.thomas@blueapron.com>
2017-09-11 19:13:17 -0400

    Currently-failing acceptance test for language-preference service

--- tests/acceptance/language-dropdown-test.js
+++ tests/acceptance/language-dropdown-test.js
@@ -1,51 +1,42 @@
-// WIP
+// // WIP
 
-// import Ember from 'ember';
-// import { module, test } from 'qunit';
-// import startApp from 'hospitalrun/tests/helpers/start-app';
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'hospitalrun/tests/helpers/start-app';
+// import { t, expectTranslation } from 'hospitalrun/node_modules/ember-i18n/test-support/helpers';
 
-// module('Acceptance | language dropdown', {
-//   beforeEach() {
-//     this.application = startApp();
-//     // this.application.inject('service', 'languagePreference', 'service:language-preference');
-//   },
+module('Acceptance | language dropdown', {
+  beforeEach() {
+    this.application = startApp();
+    // this.application.inject('service', 'languagePreference', 'service:language-preference');
+  },
 
-//   afterEach() {
-//     Ember.run(this.application, 'destroy');
-//   }
-// });
+  afterEach() {
+    Ember.run(this.application, 'destroy');
+  }
+});
 
-// test('setting a language preference persists after logout', (assert) => {
-//   runWithPouchDump('default', () => {
-//     authenticateUser({ name: 'foobar', prefix: 'p2' });
-//     visit('/');
+test('setting a language preference persists after logout', (assert) => {
+  runWithPouchDump('default', () => {
+    authenticateUser({ name: 'foobar', prefix: 'p2' });
+    visit('/');
 
-//     andThen(() => {
-//       assert.equal(currentURL(), '/');
-//       click('a.settings-trigger');
-//     });
-//     andThen(() => {
-//       select('.language-dropdown', 'German');
-//     });
-    // andThen(() => {
-    //   assert.equal(find('.view-current-title').text(), 'Was möchten Sie tun?');
-    // });
-    // andThen(() => {
-    //   click('a.settings-trigger');
-    // });
-    // andThen(() => {
-    //   click('a.logout');
-    // });
-    // andThen(() => {
-    //   authenticateUser({ name: 'foobar' });
-    //   visit('/');
-    // });
-    // andThen(() => {
-    //   assert.equal(find('.view-current-title').text(), 'Was möchten Sie tun?');
-    // });
-
-//   });
-// });
+    andThen(() => {
+      assert.equal(currentURL(), '/');
+      assert.equal(find('.view-current-title').text(), 'Welcome to HospitalRun!');
+    });
+    andThen(() => {
+      click('a.settings-trigger');
+      waitToAppear('.settings-nav');
+      // andThen(() => {
+      //   select('.language-dropdown', 'French');
+      //   andThen(() => {
+      //     debugger;
+      //   });
+      // });
+    });
+  });
+});
 
 // test('different users can have different language preferences on the same browser', function(assert) {
 // });
